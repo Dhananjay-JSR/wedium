11a802a748d18c1db23d25e4e1d4bd446643a604
Dhananjay-JSR <ajaysenday@gmail.com>
2023-12-13 00:02:25 +0530

    BootStrap

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,16 @@
 import express from "express"
+import process from "process"
 import {PORT} from "./index"
+import path from "path"
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
+app.use(express.static(path.join(process.cwd(), "..", "client", "dist")));
 
+
+app.get("/api",(req,res)=>{
+  res.send("Hello World")
+})
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
+
